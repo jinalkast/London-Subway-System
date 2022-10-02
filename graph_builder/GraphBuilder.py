4b501f4b4e858fd0b0e461456d7976a89a5dc578
Maged Armanios <maged.armanios@yahoo.com>
2022-10-02 18:17:08 -0400

    Removed week 3 to make tag

--- graph_builder/GraphBuilder.py
+++ graph_builder/GraphBuilder.py
@@ -50,67 +50,6 @@ class GraphBuilder():
                                int(row[2]), float(row[3]))
         return g
 
-    def buildRandomGraph(amount_of_nodes, degree, uniform=True):
-        g = Graph()
-        # Crate vertices
-        for i in range(1, amount_of_nodes + 1):
-            g.add_station(i, 51 + random.random(), -random.random(),
-                          Util.nameGenerator(), random.randint(1, 5))
-
-        # Doesn't add edges if there aren't any other vertices
-        # to connect to
-        if amount_of_nodes < 2:
-            return g
-
-        # Add edges
-        for i in g.graph:
-
-            if uniform:
-                # Keep adding edges till degree is met
-                while len(g.graph[i].connections) != degree:
-                    # First seek out other vertices missing connections
-                    for j in g.graph:
-                        # Make sure it isn't the same vertice as itself
-                        if j != i and len(g.graph[j].connections) != degree:
-                            # Generate random line and edge weight
-                            line = random.randint(1, 4)
-                            edge_weight = random.randint(1, 3)
-                            g.add_line(i, j, line, edge_weight)
-                            g.add_line(j, i, line, edge_weight)
-
-                        # Break if i has enough connections
-                        if len(g.graph[i].connections) == degree:
-                            break
-
-                    # Runs if every node has enough connections already
-                    if len(g.graph[i].connections) != degree:
-                        # Makes connections with random vertices till it has
-                        # enough
-                        while len(g.graph[i].connections) != degree:
-                            random_station = random.randint(1, amount_of_nodes)
-                            # Make sure it isn't the same vertice as itself
-                            if random_station != i:
-                                line = random.randint(1, 4)
-                                edge_weight = random.randint(1, 3)
-                                g.add_line(
-                                    i, random_station, line, edge_weight)
-                                g.add_line(
-                                    random_station, i, line, edge_weight)
-
-            # Non uniform distribution
-            # Each vertice makes the required amount of connections to a random
-            # node
-            else:
-                while len(g.graph[i].connections) != degree:
-                    random_station = random.randint(1, amount_of_nodes)
-                    if random_station != i:
-                        line = random.randint(1, 4)
-                        edge_weight = random.randint(1, 3)
-                        g.add_line(i, random_station, line, edge_weight)
-                        g.add_line(random_station, i, line, edge_weight)
-
-        return g
-
     def buildComponentGraph(graph, connectedComponents):
         cc = connectedComponents
         graph_of_components = Graph()
